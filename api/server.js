352ffe62d656927d3d40d9eaa6ce27bcead9fcda
Kat <kyevsukov@gmail.com>
2021-10-25 13:31:45 -0400

    can send dog by id

--- api/server.js
+++ api/server.js
@@ -20,10 +20,27 @@ server.get('/api/dogs', async (req, res) => {
         res.status(200).json(dogs)
         // throw new Error('ERROR FETCHING DOGS!')
     } catch (error) {
+        console.log(error.message) // log statement!!
         res.status(500).json({ message: `Error! ${error.message}`})
     }
 })
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.get('/api/dogs/:id', async (req, res) => {
+    try {
+      // pull that third segment of the path
+      // because that is the id we need
+      const { id } = req.params
+      const dog = await Dog.findById(id)
+      if (!dog) {
+        res.status(404).json({ message: `dog ${id} not found`})
+      } else {
+        res.status(200).json(dog)
+      }
+    } catch (error) {
+      res.status(500).json({ message: `Argh!!! ${error.message}`})
+    }
+  })
+
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
