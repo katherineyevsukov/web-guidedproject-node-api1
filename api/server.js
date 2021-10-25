a59c15b22ba290d18a4dd92499c513ab37ec0d11
Kat <kyevsukov@gmail.com>
2021-10-25 13:11:39 -0400

    first commit

--- api/server.js
+++ api/server.js
@@ -1,16 +1,34 @@
 // IMPORTS AT THE TOP
-
+const express = require('express')
+const Dog = require('./dog-model')
 // INSTANCE OF EXPRESS APP
-
-// GLOBAL MIDDLEWARE
-
+const server = express()
+// GLOBAL MIDDLEWARE/ VERY IMPORTANT!!
+server.use(express.json())
 // ENDPOINTS
 
 // [GET] / (Hello World endpoint)
+//req(represents request from client)
+//res(the response we put together)
+server.get('/', (req, res) => {
+    res.json({ message: 'hello, world' })
+})
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
+server.get('/api/dogs', async (req, res) => {
+    try {
+        const dogs = await Dog.findAll() //trip to db using an async helper function, db operations are always async
+        res.status(200).json(dogs)
+        // throw new Error('ERROR FETCHING DOGS!')
+    } catch (error) {
+        res.status(500).json({ message: `Error! ${error.message}`})
+    }
+})
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
+module.exports = server
+
+//nodemon restarts the server whenever we make changes
